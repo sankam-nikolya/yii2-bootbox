00b60bbe3d2c4ea1522ec1686be7d12a3289dde8
Nikolya <k_m_i@ukr.net>
2017-12-04 16:57:55 +0200

    script minify add

--- BootboxOverrideAsset.php
+++ BootboxOverrideAsset.php
@@ -11,13 +11,15 @@ class BootboxOverrideAsset extends BootboxAsset
     {
         parent::registerAssetFiles($view);
 
-        $view->registerJs('
+        $script = '
             yii.confirm = function(message, ok, cancel) {
                 bootbox.confirm(message, function(result) {
                     if (result) { !ok || ok(); } else { !cancel || cancel(); }
                 });
             }
-        ');
+        ';
+
+        $view->registerJs(preg_replace('/(\t+|\n|\s{2,})/', '', $script));
 
         if (Yii::$app->language !== null && strlen(Yii::$app->language) >= 2) {
             $view->registerJs('bootbox.setDefaults({locale: "' . substr(Yii::$app->language, 0, 2) . '"});');
